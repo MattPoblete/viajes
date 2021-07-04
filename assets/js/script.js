5a4710304470c174cacaabd902e1631a9a6d2f66
Matias <matias.pobleteduran@gmail.com>
2021-07-04 19:42:51 -0400

    Pagina principal completa con formulario funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 $( document ).ready(function() {
+    
+    //Smooth scroll
+    $("#navbar a").on("click", (function (evento) { 
+        evento.preventDefault();
+        let elemento = $(this).attr("href");
+      console.log(elemento);
+      $('html, body').animate({scrollTop: $(elemento).offset().top}, 1500);
+      }));
+      
+    //desactivar pausa del carrusel en hover y modificacion de intervalo  
     $('.carousel').carousel({
         pause : false,
         interval: 3500
