7c8fb5d11bf0693fba779d46d9a1ce0e2c550b3c
Matias <matias.pobleteduran@gmail.com>
2021-07-04 20:25:33 -0400

    Proyecto listo

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,22 @@ $( document ).ready(function() {
       console.log(elemento);
       $('html, body').animate({scrollTop: $(elemento).offset().top}, 1500);
       }));
-      
+
     //desactivar pausa del carrusel en hover y modificacion de intervalo  
     $('.carousel').carousel({
         pause : false,
         interval: 3500
     })
+    //modals//
+    $('#btn-card1').click(function(){
+        $('#modal1').modal('show')
+    })
+    $('#btn-card2').click(function(){
+        $('#modal2').modal('show')
+    })
+    $('#btn-card3').click(function(){
+        $('#modal3').modal('show')
+    })
+    $('#btn-card4').click(function(){
+        $('#modal4').modal('show')})
   });
